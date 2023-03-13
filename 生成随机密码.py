c6903f1dd2d0cce2d6fe8b88019ce52b670e843a
LindseyElder <96710011+LindseyElder@users.noreply.github.com>
2023-03-13 21:40:12 +0800

    Add files via upload

---
+++ 生成随机密码.py
@@ -0,0 +1,24 @@
+import random
+import hashlib
+
+# 获取用户提供的种子字符串
+user_seed = input("请输入种子：")
+
+# 使用SHA-512算法计算种子的哈希值，并将其作为随机数生成器的种子值
+random.seed(hashlib.sha512(user_seed.encode()).digest())
+
+# 定义密码生成函数
+def generate_password(length):
+    # 密码字符集
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*"
+    # 从字符集中随机选择指定长度的字符，并将它们连接起来生成密码
+    password = "".join(random.choice(chars) for _ in range(length))
+    return password
+
+# 生成10个长度为8的随机密码
+for i in range(1):
+    password = generate_password(20)
+    print(password)
+
+
+
